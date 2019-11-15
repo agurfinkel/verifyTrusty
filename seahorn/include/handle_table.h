c23dc8bfc0267b1b0e557fd2726fd1836cdb4034
Xiang Zhou <x245zhou@uwaterloo.ca>
2019-11-15 12:28:04 -0500

    rewrote handle table to keep global ptr instead

--- seahorn/include/handle_table.h
+++ seahorn/include/handle_table.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <trusty_ipc.h> // for INVALID_IPC_HANDLE and handle_t
 #include "seahorn/seahorn.h"
+#include <stdlib.h>
 #ifndef __cplusplus
 #include <stdbool.h>
 #endif
@@ -10,7 +11,7 @@ Single class for holding a map of handles
 #ifdef __cplusplus
 extern "C" {
 #endif
-struct handle_table {
+typedef struct handle_table {
     // In keymaster_ipc, assuming handles are treated one by one,
     // each either "handled" or removed by close(),
     // so one handle_t each for port and channel is sufficient
@@ -25,8 +26,9 @@ struct handle_table {
     handle_t chan_handle;
     bool chan_handle_active;
     void* chan_cookie;
-};
+} handle_table_t;
 
+void handle_table_init(handle_t secure_port, handle_t non_secure_port, handle_t channel);
 void add_handle(handle_t handle);
 void remove_handle(handle_t handle);
 bool contains_handle(handle_t handle);
--- seahorn/lib/handle_table.c
+++ seahorn/lib/handle_table.c
@@ -1,95 +1,100 @@
 #include "handle_table.h"
 
 // only allow access from functions within this file
-static struct handle_table ht = {
-    .secure_port_handle = INVALID_IPC_HANDLE,
-    .secure_port_handle_active = false,
-    .secure_port_cookie = NULL,
+static handle_table_t* ht;
 
-    .non_secure_port_handle = INVALID_IPC_HANDLE,
-    .non_secure_port_handle_active = false,
-    .non_secure_port_cookie = NULL,
+void handle_table_init(handle_t secure_port, handle_t non_secure_port, handle_t channel) {
+    if (ht) {
+        return;
+    }
+    ht = calloc(1, sizeof(ht));
 
-    .chan_handle = INVALID_IPC_HANDLE,
-    .chan_handle_active = false,
-    .chan_cookie = NULL,
-};
+    ht->secure_port_handle = secure_port;
+    ht->secure_port_handle_active = secure_port != INVALID_IPC_HANDLE;
+
+    ht->non_secure_port_handle = non_secure_port;
+    ht->non_secure_port_handle_active = non_secure_port != INVALID_IPC_HANDLE;
+
+    ht->chan_handle = channel;
+    ht->chan_handle_active = channel != INVALID_IPC_HANDLE;
+    return;
+}
 
 void add_handle(handle_t handle) {
     if (handle & 0x2) { // port
-        // assume(!ht.port_handle_active);
+        // assume(!ht->port_handle_active);
         if (handle & 0x1) { // secure port
-            ht.secure_port_handle = handle;
-            ht.secure_port_handle_active = true;
+            ht->secure_port_handle = handle;
+            ht->secure_port_handle_active = true;
         } else {
-            ht.non_secure_port_handle = handle;
-            ht.non_secure_port_handle_active = true;
+            ht->non_secure_port_handle = handle;
+            ht->non_secure_port_handle_active = true;
         }
    } else {
-        // assume(!ht.chan_handle_active);
-        ht.chan_handle = handle;
-        ht.chan_handle_active = true;
+        // assume(!ht->chan_handle_active);
+        ht->chan_handle = handle;
+        ht->chan_handle_active = true;
    }
 }
 
 void remove_handle(handle_t handle) {
-    if (handle == ht.secure_port_handle)
+    if (handle == ht->secure_port_handle)
     {
-        ht.secure_port_handle_active = false;
+        ht->secure_port_handle_active = false;
     }
-    else if (handle == ht.non_secure_port_handle)
+    else if (handle == ht->non_secure_port_handle)
     {
-        ht.non_secure_port_handle_active = false;
+        ht->non_secure_port_handle_active = false;
     }
-    else if (handle == ht.chan_handle)
+    else if (handle == ht->chan_handle)
     {
-        ht.chan_handle_active = false;
+        ht->chan_handle_active = false;
     }
 }
 
 bool contains_handle(handle_t handle) {
-    if (handle == ht.secure_port_handle)
+    if (handle == ht->secure_port_handle)
     {
-        return ht.secure_port_handle_active;
+        return ht->secure_port_handle_active;
     }
-    else if (handle == ht.non_secure_port_handle)
+    else if (handle == ht->non_secure_port_handle)
     {
-        return ht.non_secure_port_handle_active;
+        return ht->non_secure_port_handle_active;
     }
-    else if (handle == ht.chan_handle)
+    else if (handle == ht->chan_handle)
     {
-        return ht.chan_handle_active;
+        return ht->chan_handle_active;
     }
     else return false;
 }
 
 void* get_handle_cookie(handle_t handle) {
-    if (handle == ht.secure_port_handle)
+    if (handle == ht->secure_port_handle)
     {
-        return ht.secure_port_cookie;
+        return ht->secure_port_cookie;
     }
-    else if (handle == ht.non_secure_port_handle)
+    else if (handle == ht->non_secure_port_handle)
     {
-        return ht.non_secure_port_cookie;
+        return ht->non_secure_port_cookie;
     }
-    else if (handle == ht.chan_handle)
+    else if (handle == ht->chan_handle)
     {
-        return ht.chan_cookie;
+        return ht->chan_cookie;
     }
     else return NULL;
 }
 
 void set_handle_cookie(handle_t handle, void* cookie) {
-    if (handle == ht.secure_port_handle)
+    if (handle == ht->secure_port_handle)
     {
-        ht.secure_port_cookie = cookie;
+        ht->secure_port_cookie = cookie;
     }
-    else if (handle == ht.non_secure_port_handle)
+    else if (handle == ht->non_secure_port_handle)
     {
-        ht.non_secure_port_cookie = cookie;
+        ht->non_secure_port_cookie = cookie;
     }
-    else if (handle == ht.chan_handle)
+    else if (handle == ht->chan_handle)
     {
-        ht.chan_cookie = cookie;
+        ht->chan_cookie = cookie;
     }
 }
