93e471d8b14a564b4b805295d34029d18fa8a607
Xiang Zhou <x245zhou@uwaterloo.ca>
2019-11-20 17:15:40 -0500

    void unused params in case -Wunused applied anywhere

--- seahorn/stubs/trusty_msg.c
+++ seahorn/stubs/trusty_msg.c
@@ -12,6 +12,7 @@
 
 /* Redefine trusty messaging APIs */
 int get_msg(handle_t handle, ipc_msg_info_t *msg_info) {
+    (void) handle;
     int retval = nd_int();
     size_t msg_len = (size_t) nd_unsigned();
     uint32_t msg_id = (uint32_t) nd_unsigned();
@@ -30,6 +31,9 @@ int get_msg(handle_t handle, ipc_msg_info_t *msg_info) {
 ssize_t read_msg(handle_t handle, uint32_t msg_id, uint32_t offset, ipc_msg_t *msg) {
     // return Total number of bytes stored in the dst buffers on success;
     // a negative error otherwise
+    (void) handle;
+    (void) msg_id;
+    (void) offset;
     ssize_t res = (ssize_t)nd_int();
     struct iovec* iv = msg->iov;
     if (res >= 0) {
@@ -48,12 +52,16 @@ ssize_t read_msg(handle_t handle, uint32_t msg_id, uint32_t offset, ipc_msg_t *m
 
 ssize_t send_msg(handle_t handle, ipc_msg_t* msg) {
     // Total number of bytes sent on success; a negative error otherwise
+    (void) handle;
+    (void) msg;
     return (ssize_t)nd_int();
 }
 
 int put_msg(handle_t handle, uint32_t msg_id) {
     // return NO_ERROR on success; a negative error otherwise
     //assume(retval == NO_ERROR || retval < 0);
+    (void) handle;
+    (void) msg_id;
     return nd_int();
 }
 
@@ -61,11 +69,10 @@ int put_msg(handle_t handle, uint32_t msg_id) {
 // current model only handles the channel event of the latest port event
 // if they happen to match
 int wait_any(uevent_t* ev, uint32_t timeout_msecs) {
+    (void) timeout_msecs;
     int ret = nd_int();
     assume(ret <= NO_ERROR && ret >= ERR_USER_BASE);
     if (ret == NO_ERROR) {
-        // simulate populating the event with no handler
-        // ev->cookie = nd_ptr();
         ev->handle = nd_int();
         ev->cookie = get_handle_cookie(ev->handle);
         assume(ev->handle > INVALID_IPC_HANDLE);
@@ -81,6 +88,9 @@ handle_t port_create(const char* path,
                      uint32_t recv_buf_size,
                      uint32_t flags)
                      {
+    (void) path;
+    (void) num_recv_bufs;
+    (void) recv_buf_size;
     handle_t retval = (handle_t)nd_int();
     if (retval < 0)
         return retval; // return error message as is
@@ -115,6 +125,7 @@ int set_cookie(handle_t handle, void* cookie) {
 // given port_handle that references a port, open a channel and
 // store channel handle to handle table
 handle_t accept(handle_t port_handle, uuid_t* peer_uuid) {
+    (void) port_handle;
     handle_t chan = (handle_t)nd_int();
     if (chan >= 0) {
         assume(!(chan & 0x2)); // is channel
