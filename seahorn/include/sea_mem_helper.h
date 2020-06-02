265e82cc4fba34bbe94a2dcaddb96127ad987d46
Arie Gurfinkel <arie.gurfinkel@uwaterloo.ca>
2020-06-02 12:09:03 -0400

    pass over realloc()

--- seahorn/include/sea_mem_helper.h
+++ seahorn/include/sea_mem_helper.h
@@ -1,14 +1,20 @@
+#pragma once
 #include <stdlib.h>
 #include <stdbool.h>
 
 // #define sea_countof(arr) maybe_store_arr_len(arr, countof(arr))
 
+/*  Return size of object pointed by \p ptr  */
 size_t sea_get_alloc_size(void *ptr);
 
+/*  Return true if the size of objected pointed to by \p ptr is known  */
 bool sea_ptr_size_stored(void *ptr);
 
+/* Return true if the length of the array pointed by \p arr is known */
 bool sea_arr_len_stored(void *arr);
 
+/* Return the length of the array pointed to by \p arr */
 size_t sea_get_arr_len(void *arr);
 
+/* TODO: figure out what this function is doing */
 size_t sea_maybe_store_arr_len(void *arr, size_t len);
--- seahorn/stubs/realloc_override.c
+++ seahorn/stubs/realloc_override.c
@@ -1,29 +1,39 @@
+/** Verify Trusty
+
+    Implementation of realloc()
+
+ */
+
 #include "sea_mem_helper.h"
 #include "nondet.h"
+
 #include <stdint.h>
 #include <stddef.h>
 
 int8_t *g_ptr0 = NULL;
 size_t g_ptr0_size;
 
-void *realloc( void *ptr, size_t new_size ) {
-    if (ptr) {
-        free(ptr);
-    }
-    ptr = malloc(new_size);
-    if (nd_store_mem_size() && g_ptr0 == NULL) {
-        g_ptr0 = (int8_t*)ptr;
-        g_ptr0_size = new_size;
-    }
-    return ptr;
+void *realloc(void *ptr, size_t new_size) {
+  if (ptr) {free(ptr);}
+
+  ptr = malloc(new_size);
+
+  /** instrumentation for memory checking properties */
+  /** TODO: factor out from generic realloc() implementation */
+  if (nd_store_mem_size() && g_ptr0 == NULL) {
+    g_ptr0 = (int8_t *)ptr;
+    g_ptr0_size = new_size;
+  }
+  return ptr;
 }
 
+/** Helper functions to get access to meta-data about allocated memory */
 bool sea_ptr_size_stored(void *ptr) {
-    return ptr != NULL && (int8_t*)ptr == g_ptr0 && g_ptr0_size > 0;
+  return ptr != NULL && (int8_t *)ptr == g_ptr0 && g_ptr0_size > 0;
 }
 
 size_t sea_get_alloc_size(void *ptr) {
-    if ((int8_t*)ptr == g_ptr0)
-        return g_ptr0_size;
-    return nd_get_alloc_size();
+  if ((int8_t *)ptr == g_ptr0)
+    return g_ptr0_size;
+  return nd_get_alloc_size();
 }
